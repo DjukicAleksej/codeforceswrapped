05b10a4e7632d4379fe81f13b919052fa84fc756
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-20 23:01:38 +0100

    Added the logic for checking if the user gave us his username or the endpoint and I built a base url which we weill use in constructing the api url which we will use to call the api

--- app/api/cfproxy/route.ts
+++ app/api/cfproxy/route.ts
@@ -12,6 +12,21 @@ export async function GET(request: Request) {
         const host =  headersList.get('host') || 'localhost';
         const protocol = process.env.NODE_ENV === 'developmeny' ? 'http' : 'https';
 
-        
+        //for parsing the url paramss
+        const fullURl = new URL(request.url);
+        const endpoint = fullURl.searchParams.get('endpoint');
+        const params = fullURl.searchParams.get('params');
+        if(!endpoint){
+            return NextResponse.json(
+                {error :'Endpoint is required'},
+                {status: 400}
+            );
+        }
+        //construct Codeforces API URL
+        const cfUrl = new URL(`${CF_API_BASE}/${endpoint}`);
+        if(params){
+            cfUrl.search = params;
+        }
+
     }
 }
