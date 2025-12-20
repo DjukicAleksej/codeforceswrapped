5387d2b116609e91a349c95f3a8a42054ec3c3a4
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-20 22:52:09 +0100

    Started writing the get equest in the cfproxy route

--- app/api/cfproxy/route.ts
+++ app/api/cfproxy/route.ts
@@ -2,3 +2,16 @@ import {NextResponse} from 'next/server';
 import { headers } from 'next/headers';
 
 const CF_API_BASE = 'https://codeforces.com/api';
+
+export const dynamic ='force-dynamic';
+export const revalidate = 0;
+
+export async function GET(request: Request) {
+    try {
+        const headersList = headers();
+        const host =  headersList.get('host') || 'localhost';
+        const protocol = process.env.NODE_ENV === 'developmeny' ? 'http' : 'https';
+
+        
+    }
+}
