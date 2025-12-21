ea8ff902605ae6c1a357e6461d240d818dc25352
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-21 18:48:36 +0100

    Added the same function from the stats because I will need it

--- app/api/wrapped/route.ts
+++ app/api/wrapped/route.ts
@@ -4,3 +4,18 @@ import { processUserStats } from "@/lib/stats";
 
 export const dynamic = 'auto';
 export const revalidate = 3600; //Revalidate every hour
+
+function getRankAndColor(rating: number) : {rank: string;color: string}{
+    if(rating === 0) return {rank: 'Unrated', color: 'text-gray-500'};
+    if (rating < 1200) return { rank: 'Newbie', color: 'text-gray-500' };
+    if (rating < 1400) return { rank: 'Pupil', color: 'text-green-500' };
+    if (rating < 1600) return { rank: 'Specialist', color: 'text-cyan-500' };
+    if (rating < 1900) return { rank: 'Expert', color: 'text-blue-500' };
+    if (rating < 2100) return { rank: 'Candidate Master', color: 'text-purple-500' };
+    if (rating < 2300) return { rank: 'Master', color: 'text-[#FFA500]' };
+    if (rating < 2400) return { rank: 'International Master', color: 'text-[#FFA500]' };
+    if (rating < 2600) return { rank: 'Grandmaster', color: 'text-[#FF0000]' };
+    if (rating < 3000) return { rank: 'International Grandmaster', color: 'text-[#FF0000]' };
+    if (rating < 4000) return { rank: 'Legendary Grandmaster', color: 'text-[#FF0000]' };
+    return { rank: 'Tourist', color: 'text-[#FF0000]' };
+}
