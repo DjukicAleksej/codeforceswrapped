062f9de9f025d250e28abe6fcd19c782e00b02c0
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-21 18:56:06 +0100

    Started writing the userinfo fnction

--- app/api/wrapped/route.ts
+++ app/api/wrapped/route.ts
@@ -19,3 +19,25 @@ function getRankAndColor(rating: number) : {rank: string;color: string}{
     if (rating < 4000) return { rank: 'Legendary Grandmaster', color: 'text-[#FF0000]' };
     return { rank: 'Tourist', color: 'text-[#FF0000]' };
 }
+
+async function getUserInfo(handle: string) {
+    try{
+        const response =await fetch (`https://codeforces.com/api/user.info?handles=${handle}`);
+        const data = await response.json();
+
+        if(data.status === 'OK' && data.result.length > 0) {
+            const user = data.result[0];
+            const currentRating = user.rating || 0;
+            const maxRating = user.maxRating || 0;
+            const currentRankInfo = getRankAndColor(currentRating);
+            const maxRankInfo = getRankAndColor(maxRating);
+
+            //ensure https and gandle both titlephoto and avatar
+            let profilePicture = user.titlePhoto || user.avatar || '';
+            if(profilePicture){
+                profilePicture = profilePicture.replace(/^http:/,'https:');
+            }
+            
+        }
+    }
+}
