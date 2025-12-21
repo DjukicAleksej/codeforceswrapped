f50127a5e7698a714c0b9699b041b34f203d470b
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-21 12:35:32 +0100

    Finished the rank and color function whic was easy as I just need to check the rank against different values from 0 , then if less than 1200 newbie all up to lgendary grandmaster , and I return the title and color of the title for my wrapped

--- lib/stats.ts
+++ lib/stats.ts
@@ -1,2 +1,19 @@
 import { CodeforcesUser,Submission,UserStats,PowerClass,RatingPoint }  from "./types";
 import {generateContributionData} from './api'
+
+
+//Utility fuctions
+
+function getRankAndColor(rating: number): {rank: string; color: string} {
+    if(!rating || rating === 0) return { rank: 'Unrated',color:'text-gray-500'};
+    if(rating < 1200) return { rank: 'Newbie',color:'text-gray-500'};
+    if(rating < 1400) return { rank: 'Pupil',color:'text-green-500'};
+    if(rating < 1600) return { rank: 'Specialist',color:'text-cyan-500'};
+    if(rating < 1900) return { rank: 'Expert',color:'text-blue-500'};
+    if(rating < 2100) return { rank: 'Candidate Master',color:'text-purple-500'};
+    if(rating < 2300) return { rank: 'Master',color:'text-[#FFA500]'};
+    if(rating < 2400) return { rank: 'International Master',color:'text-[#FFA500]'};
+    if(rating < 2600) return { rank: 'Grandmaster',color:'text-[#FF0000]'};
+    if(rating < 3000) return { rank: 'International Grandmaster',color:'text-[#FF0000]'};
+    return {rank: 'Legendary Grandmaster',color: 'text-[#FF0000]'};
+}
