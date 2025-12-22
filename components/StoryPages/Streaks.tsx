d688ab6997b50ff24aba1a63950b9fc929305deb
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-22 17:35:27 +0100

    Started working on the streets page ,a again it is similar to the rest just have to change the text and a little bit of animations , finished with the title now going to put up how long is your streak and most active month

---
+++ components/StoryPages/Streaks.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { motion } from "framer-motion";
+import { useState,useEffect } from "react";
+import { UserStats} from "@/lib/types";
+import {Flame} from "lucide-react";
+
+interface StreakProps {
+    stats: UserStats;
+    onNext: () => void;
+}
+
+export default function Streaks({ stats, onNext}: StreakProps){
+    const [isVisible,setIsVisible] = useState(false);
+    useEffect(() => {
+        setIsVisible(true);
+    }, []);
+
+    return (
+        <div className="h-screen w-full flex items-center justify-center bg-gradient-to-br from-[#1a1d24] to-black">
+            <div className="max-w-2xl w-full p-8 space-y-12">
+                <motion.div
+                initial={{opacity:0,y:20}}
+                animate={{opacity: isVisible ? 1 : 0 ,y:isVisible ? 0 : 20}}
+                transition={{duration: 0.8}}
+                className="text-center"
+
+                >
+                    <h1 className="text-4xl font-bold text-white mb-4">Consistency is Key</h1>
+                    <p className="text-gray-400">Your dedication shows in the numbers</p>
+                </motion.div>
+
+                
+            </div>
+        </div>
+    )
+}
