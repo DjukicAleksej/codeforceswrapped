968724c11e55ebeab5431ff0bb639beaca8f47a6
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-22 15:08:34 +0100

    Added the page.tsx file and imported all the components I need for this page that will be the main page user sees after entering the site

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { useState } from 'react';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import {Card, CardHeader , CardTitle , CardDescription,CardContent} from '@/components/ui/card';
+import {Badge} from '@/components/ui/badge';
+import {Avatar} from '@/components/ui/avatar';
+import Image from 'next/image';
+import { useRouter} from 'next/navigation';
+
+
+
