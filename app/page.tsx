cd290fecf3776525f8006acbbc11e6684d8371a5
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-22 15:26:05 +0100

    After long errors and searching the new I realized the toast shadcn component was deprecated wo I swtiched to the sonner one and now there is no error , now going to continue buildingthe main page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,20 @@ import {Badge} from '@/components/ui/badge';
 import {Avatar} from '@/components/ui/avatar';
 import Image from 'next/image';
 import { useRouter} from 'next/navigation';
+import { Trophy,ChevronRight} from 'lucide-react';
+import Link from 'next/link';
+import {toast,Toaster} from 'sonner';
 
+export default function Home(){
+    const [handle,setHandle] = useState('');
+    const [loading,setLoading] = useState(false);
+    const router = useRouter();
+    const [showCopied,setShowCopied] = useState(false);
 
+    const generateWrapped = async (e: React.FormEvent) => {
+        e.preventDefault();
+        if(!handle){
 
+        }
+    }
+}
