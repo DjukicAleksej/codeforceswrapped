ba7cb96b0f911139b527b8e67bd4f5950b0d0b3e
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-23 12:56:26 +0100

    Finished the root layer of the file andwrapped evertything on the themeprovider just ilke it said on the website

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,3 +31,24 @@ export const metadata: Metadata = {
   },
 };
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head />
+      <body className={inter.className}>
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        >
+          {children}
+        <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
